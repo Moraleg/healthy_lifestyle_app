79cff290bfbbae166f69df380fa1c1a360031e88
Geraldine Morales <geraldine.morales02@gmail.com>
2017-05-24 12:39:21 -0700

    created server.js file and basic file structure

---
+++ server.js
@@ -0,0 +1,33 @@
+// MAIN SERVER //
+
+// DEPENDENCIES //
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/project_3';
+var db = mongoose.connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=================== DB CONNECTION ===============
+mongoose.connect(mongoUri);
+db.once('open', function() {
+  console.log('final_project connected to MongoDB!');
+});
+
+//================ SERVER CONNECTION ===============
+
+app.listen(port, function() {
+  console.log('server is listening on port:', port);
+});
