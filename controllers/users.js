965665976c79edea1d6155a2bdfad2ada6413bb1
Geraldine Morales <geraldine.morales02@gmail.com>
2017-05-25 22:59:25 -0700

    added user sign-up angular route and sign-up form

--- controllers/users.js
+++ controllers/users.js
@@ -39,7 +39,7 @@ router.get('/:id', function(req, res) {
 });
 
 /* User Post Route */
-//tested with curl -> forbidden
+//tested with curl 
 router.post('/', function(req, res){
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)); //encrypts password
   User.create(req.body, function(err, createdUser){ // creates user
--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -3,15 +3,66 @@ console.log('final_project app.js connected!');
 var app = angular.module('MyApp', []);
 
 app.controller('mainController', ['$http', '$scope', function($http, $scope) {
+  // this.test = 'mainController is working!';
 
-  this.test = 'mainController is working!';
+  // Variables //
+  const controller = this;
+  this.username = '';
+  this.password = '';
+  this.confirmPassword = '';
+  this.showMsg = false;
+  this.msgContent = '';
 
+  // this.submitLogin = function() {
+  //   this.showMsg = false;
+  //   this.msgContent = '';
+  // };
 
+  // USER SIGNUP //
+  this.createAccount = function() {
+    if (this.password === this.confirmPassword) {
+      $http({
+        method: 'POST',
+        url: '/users',
+        data: {
+          username: this.username,
+          password: this.password
+        }
+      }).then(function(response) {
+        console.log(response);
+        if(!response.data.errmsg) {
+          console.log('account successfully created!');
+          controller.msgContent = 'Your account has been created! Please log in to continue.';
+          controller.showMsg = true;
+        } else if (response.data.code === 11000){
+          controller.msgContent = 'Sorry, the username is already taken. Please try again';
+          controller.showMsg = true;
+        } else {
+          controller.msgContent = 'Sorry, something went wrong. Please try again.';
+          controller.showMsg = true;
+        }
+        controller.username = '';
+        controller.password = '';
+        controller.confirmPassword = '';
+      }, function(error) {
+        console.log(error);
+        controller.msgContent = 'Sorry, something went wrong. Please try again.';
+        controller.displayMessage = true;
+        controller.username = '';
+        controller.password = '';
+        controller.confirmPassword = '';
+      });
+    } else {
+      ctrl.msgContent = 'Sorry, the passwords you entered did not match.';
+      ctrl.displayMessage = true;
+    }
+  }; //End createAccount
 
+  // USER LOGIN //
 
+  $http({
 
-
-
+  })
 
 
 
